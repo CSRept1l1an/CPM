660af530f4cf4781a1850991868f08d3de47bdb7
Telman Yusifov <csyusifovtelman@gmail.com>
2024-05-18 12:33:42 +0400

    test

---
+++ database.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS users (
+               id INTEGER PRIMARY KEY,
+               username TEXT NOT NULL,
+               email TEXT NOT NULL
+            )''')
+
+cur.execute("INSERT INTO users (username, email) VALUES (?, ?)", ('john_doe', 'john@example.com'))
+cur.execute("INSERT INTO users (username, email) VALUES (?, ?)", ('jane_doe', 'jane@example.com'))
+
+conn.commit()
+
+cur.execute("SELECT * FROM users")
+print("Users:")
+for row in cur.fetchall():
+    print(row)
+
+cur.close()
+conn.close()
