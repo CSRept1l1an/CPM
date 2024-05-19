8ff8777b3fb9120216a531b2c3a268cc30b51be1
Telman Yusifov <csyusifovtelman@gmail.com>
2024-05-19 12:08:52 +0400

    test

--- criticalData.py
+++ criticalData.py
@@ -1,22 +1,69 @@
 import sqlite3
-
-database = 'management.db'
-
-
-def critical_log(data):
-
-    conn = sqlite3.connect(data)
-    c = conn.cursor()
-
-    c.execute("SELECT * FROM IT_Node_KPIs WHERE Category ='critical'")
-
-    critical_nodes = c.fetchall()
-
-    for node in critical_nodes:
-        print(node)
-
-    c.close()
-    conn.close()
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
 
 
-critical_log(database)
+class DataDisplayApp(App):
+    def build(self):
+        conn = sqlite3.connect('management.db')
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM IT_Node_KPIs WHERE Category ='critical'")
+        rows = c.fetchall()
+        conn.close()
+
+        layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
+
+        title_label = Label(
+            text="Data Display",
+            font_size='24sp',
+            bold=True,
+            size_hint=(1, None),
+            height=40,
+            halign='center',
+            valign='middle'
+        )
+        title_label.bind(size=title_label.setter('text_size'))
+        layout.add_widget(title_label)
+
+        grid = GridLayout(cols=2, spacing=20, size_hint_y=None, padding=[10, 10, 10, 10])
+        grid.bind(minimum_height=grid.setter('height'))
+
+        labels = ['ID', 'Timestamp', 'Value 1', 'Value 2', 'Value 3', 'Value 4', 'Value 5', 'Status']
+
+        for row in rows:
+            for label, value in zip(labels, row):
+                grid.add_widget(Label(
+                    text=label,
+                    font_size='18sp',
+                    halign='left',
+                    valign='middle',
+                    size_hint_y=None,
+                    height=40
+                ))
+                value_label = Label(
+                    text=str(value),
+                    font_size='18sp',
+                    halign='left',
+                    valign='middle',
+                    size_hint_y=None,
+                    height=40
+                )
+                value_label.bind(size=value_label.setter('text_size'))
+                grid.add_widget(value_label)
+
+        scroll_view = ScrollView(size_hint=(1, 1))
+        scroll_view.add_widget(grid)
+
+        layout.add_widget(scroll_view)
+
+        return layout
+
+
+if __name__ == '__main__':
+    Window.size = (400, 600)
+    DataDisplayApp().run()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -12,6 +14,10 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.widget import Widget
 from kivy.graphics.texture import Texture
 from kivy.animation import Animation
+import matplotlib.pyplot as plt
+from kivy.uix.image import Image
+
+from GraPhics import create_graph
 
 
 class GradientWidget(Widget):
@@ -33,13 +39,11 @@ class GradientWidget(Widget):
     def update_texture(self):
         self.rect1.texture = self.get_texture(self.color_1)
 
-    @staticmethod
-    def get_texture(color):
+    def get_texture(self, color):
         texture = Texture.create(size=(1, 1))
         buf = bytes([int(c * 255) for c in color])
         texture.blit_buffer(buf, colorfmt='rgba', bufferfmt='ubyte')
         return texture
-
 
 class LoginPage(Screen):
     def __init__(self, **kwargs):
@@ -104,7 +108,6 @@ class LoginPage(Screen):
             anim.cancel(self.login_button)  # Остановка предыдущей анимации
             anim.start(self.login_button)
 
-
 class MainPage(Screen):
     def __init__(self, **kwargs):
         super(MainPage, self).__init__(**kwargs)
@@ -132,21 +135,77 @@ class MainPage(Screen):
         self.menu.add_widget(quit_button)
         self.layout.add_widget(self.menu)
 
-        message_label = Label(text='Добро пожаловать на главную страницу!', size_hint=(1, 1), color=(0, 0, 1, 1),
-                              pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        self.layout.add_widget(message_label)
+        # Create main layout with three vertical sub-layouts
+        # Create main layout with three vertical sub-layouts
+        self.main_layout = BoxLayout(orientation='vertical', spacing=10, size_hint=(1, 1),
+                                     pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        self.create_bandwidth_graph()
+        self.create_cpu_graph()
+        self.create_throughput_graph()
+
+    def create_bandwidth_graph(self):
+        connection = sqlite3.connect('management.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT timestamp, bandwidth_utilization FROM Network_KPIs')
+        data = [row[1] for row in cursor.fetchall()]
+        graph = create_graph('Bandwidth Utilization', data, 'Bandwidth Utilization')
+
+        # Wrap the graph in a BoxLayout with fixed size
+        graph_layout = BoxLayout(size_hint=(None, None), size=(600, 250), spacing=50, padding=50)
+        graph_layout.add_widget(graph)
+        graph_layout.pos_hint = {'center_x': 0.5, 'center_y': 0.6}
+
+        self.layout.add_widget(graph_layout)
+        connection.close()
+
+    def create_cpu_graph(self):
+        connection = sqlite3.connect('management.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT timestamp, cpu_utilization FROM IT_Node_KPIs')
+        data = [row[1] for row in cursor.fetchall()]
+        graph = create_graph('CPU Utilization', data, 'CPU Utilization')
+
+        # Wrap the graph in a BoxLayout with fixed size
+        graph_layout = BoxLayout(size_hint=(None, None), size=(600, 250), spacing=50, padding=50)
+        graph_layout.add_widget(graph)
+        graph_layout.pos_hint = {'center_x': 0.5, 'center_y': 0.4}
+
+        self.layout.add_widget(graph_layout)
+        connection.close()
+
+    def create_throughput_graph(self):
+        connection = sqlite3.connect('management.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT timestamp, throughput FROM Application_KPIs')
+        data = [row[1] for row in cursor.fetchall()]
+        graph = create_graph('Throughput', data, 'Throughput')
+
+        # Wrap the graph in a BoxLayout with fixed size
+        graph_layout = BoxLayout(size_hint=(None, None), size=(600, 250), spacing=50, padding=50)
+        graph_layout.add_widget(graph)
+        graph_layout.pos_hint = {'center_x': 0.5, 'center_y': 0.2}
+
+        self.layout.add_widget(graph_layout)
+        connection.close()
 
     def add_top_bar(self):
         self.top_bar = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50, pos_hint={'top': 1})
         self.layout.add_widget(self.top_bar)
 
         with self.top_bar.canvas.before:
-            Color(1, 0, 0, 1)  # Red color
+            Color(1, 0, 0, 1)
             self.rect = Rectangle(size=self.top_bar.size, pos=self.top_bar.pos)
             self.top_bar.bind(size=self.update_rect, pos=self.update_rect)
 
         xti_app_label = Label(text='XTI App', color=(1, 1, 1, 1), font_size='20sp')
         self.top_bar.add_widget(xti_app_label)
+
+    def add_vertical_layout(self, title, on_press_callback):
+        box = BoxLayout(orientation='vertical', spacing=10, padding=10, size_hint=(1, 0.33))
+        button = Button(text=title, size_hint=(1, None), height=40)
+        button.bind(on_press=on_press_callback)
+        box.add_widget(button)
+        self.main_layout.add_widget(box)
 
     def update_rect(self, instance, value):
         self.rect.size = instance.size
@@ -163,26 +222,75 @@ class MainPage(Screen):
             anim = Animation(pos_hint={'x': -1}, duration=0.3)
         anim.start(self.menu)
 
-    @staticmethod
-    def switch_to_alert(instance):
+    def switch_to_alert(self, instance):
         app = App.get_running_app()
         app.root.current = 'alert_page'
 
-    @staticmethod
-    def switch_to_about(instance):
+    def switch_to_about(self, instance):
         app = App.get_running_app()
         app.root.current = 'about_page'
 
-    @staticmethod
-    def quit_app(instance):
+    def switch_to_layout1(self, instance):
+        app = App.get_running_app()
+        app.root.current = 'layout1_page'
+
+    def switch_to_layout2(self, instance):
+        app = App.get_running_app()
+        app.root.current = 'layout2_page'
+
+    def switch_to_layout3(self, instance):
+        app = App.get_running_app()
+        app.root.current = 'layout3_page'
+
+    def quit_app(self, instance):
         App.get_running_app().stop()
 
+class NetworkPage(Screen):
+    def __init__(self, **kwargs):
+        super(NetworkPage, self).__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', padding=10)
+        layout.add_widget(Label(text='Network Page'))
+        back_button = Button(text='Back to Main Page', size_hint=(0.5, None), height=50, pos_hint={'center_x': 0.5})
+        back_button.bind(on_press=self.back_to_main)
+        layout.add_widget(back_button)
+        self.add_widget(layout)
+
+    def back_to_main(self, instance):
+        app = App.get_running_app()
+        app.root.current = 'main_page'
+
+class ITNodesPage(Screen):
+    def __init__(self, **kwargs):
+        super(ITNodesPage, self).__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', padding=10)
+        layout.add_widget(Label(text='IT Nodes Page'))
+        back_button = Button(text='Back to Main Page', size_hint=(0.5, None), height=50, pos_hint={'center_x': 0.5})
+        back_button.bind(on_press=self.back_to_main)
+        layout.add_widget(back_button)
+        self.add_widget(layout)
+
+    def back_to_main(self, instance):
+        app = App.get_running_app()
+        app.root.current = 'main_page'
+
+class AppPage(Screen):
+    def __init__(self, **kwargs):
+        super(AppPage, self).__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical', padding=10)
+        layout.add_widget(Label(text='App Page'))
+        back_button = Button(text='Back to Main Page', size_hint=(0.5, None), height=50, pos_hint={'center_x': 0.5})
+        back_button.bind(on_press=self.back_to_main)
+        layout.add_widget(back_button)
+        self.add_widget(layout)
+
+    def back_to_main(self, instance):
+        app = App.get_running_app()
+        app.root.current = 'main_page'
 
 class AlertPage(Screen):
     def __init__(self, **kwargs):
         super(AlertPage, self).__init__(**kwargs)
 
-        self.rect = None
         self.layout = FloatLayout(size=(Window.width, Window.height))
         self.add_widget(self.layout)
         self.add_top_bar()
@@ -210,6 +318,7 @@ class AlertPage(Screen):
         self.layout.add_widget(message_label)
 
     def add_top_bar(self):
+        # Create a top bar with a red line and XTI App label
         self.top_bar = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50, pos_hint={'top': 1})
         self.layout.add_widget(self.top_bar)
 
@@ -236,18 +345,15 @@ class AlertPage(Screen):
             anim = Animation(pos_hint={'x': -1}, duration=0.3)
         anim.start(self.menu)
 
-    @staticmethod
-    def switch_to_main(instance):
+    def switch_to_main(self, instance):
         app = App.get_running_app()
         app.root.current = 'main_page'
 
-    @staticmethod
-    def switch_to_about(instance):
+    def switch_to_about(self, instance):
         app = App.get_running_app()
         app.root.current = 'about_page'
 
-    @staticmethod
-    def quit_app(instance):
+    def quit_app(self, instance):
         App.get_running_app().stop()
 
 
@@ -255,8 +361,6 @@ class AboutPage(Screen):
     def __init__(self, **kwargs):
         super(AboutPage, self).__init__(**kwargs)
 
-        self.rect = None
-        self.top_bar = None
         self.layout = FloatLayout(size=(Window.width, Window.height))
         self.add_widget(self.layout)
 
@@ -276,9 +380,6 @@ class AboutPage(Screen):
         self.menu.add_widget(Button(text='Dashboard', on_press=self.switch_to_main))
         self.menu.add_widget(Button(text='Alert', on_press=self.switch_to_alert))
         self.menu.add_widget(Button(text='Home', on_press=self.switch_to_main))
-
-
-
         quit_button = Button(text='Quit')
         quit_button.bind(on_press=self.quit_app)
         self.menu.add_widget(quit_button)
@@ -304,6 +405,7 @@ class AboutPage(Screen):
         self.bg_rect.pos = instance.pos
 
     def add_top_bar(self):
+        # Create a top bar with a red line and XTI App label
         self.top_bar = BoxLayout(orientation='horizontal', size_hint=(1, None), height=50, pos_hint={'top': 1})
         self.layout.add_widget(self.top_bar)
 
@@ -326,32 +428,77 @@ class AboutPage(Screen):
             anim = Animation(pos_hint={'x': 0}, duration=0.3)
         anim.start(self.menu)
 
-    @staticmethod
-    def switch_to_main(instance):
+    def switch_to_main(self, instance):
         app = App.get_running_app()
         app.root.current = 'main_page'
 
-    @staticmethod
-    def switch_to_alert(instance):
+    def switch_to_alert(self, instance):
         app = App.get_running_app()
         app.root.current = 'alert_page'
 
-    @staticmethod
-    def quit_app(instance):
+    def quit_app(self, instance):
         App.get_running_app().stop()
 
+
+class Layout1Page(Screen):
+    def __init__(self, **kwargs):
+        super(Layout1Page, self).__init__(**kwargs)
+        self.add_widget(Label(text='Layout 1 Page'))
+
+        # Add button with custom graph icon and text to return to the home page
+        home_button = Button(size_hint=(None, None), size=(120, 70), pos_hint={'top': 1, 'left': 1},
+                             background_normal='', background_color=(0, 0, 0, 0))
+        home_button.bind(on_press=self.go_to_main_page)
+
+        # Set button background color to gray
+        with home_button.canvas.before:
+            Color(0.5, 0.5, 0.5, 1)  # Gray color
+            self.bg_rect = Rectangle(size=home_button.size, pos=home_button.pos)
+
+        # Create a layout for button content
+        button_layout = BoxLayout(orientation='horizontal', spacing=5)
+
+        # Add image to the button layout
+        home_button_image = Image(source='graph_icon.png', size_hint=(None, None), size=(40, 40))
+        button_layout.add_widget(home_button_image)
+
+        # Add text to the button layout
+        home_button_text = Label(text='Home', size_hint=(None, None), size=(60, 50), font_size=18)
+        button_layout.add_widget(home_button_text)
+
+        # Add button layout to home_button
+        home_button.add_widget(button_layout)
+
+        self.add_widget(home_button)
+
+
+    def go_to_main_page(self, instance):
+        app = App.get_running_app()
+        app.root.current = 'main_page'
+
+class Layout2Page(Screen):
+    def __init__(self, **kwargs):
+        super(Layout2Page, self).__init__(**kwargs)
+        self.add_widget(Label(text='Layout 2 Page'))
+
+class Layout3Page(Screen):
+    def __init__(self, **kwargs):
+        super(Layout3Page, self).__init__(**kwargs)
+        self.add_widget(Label(text='Layout 3 Page'))
 
 class AuthApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(LoginPage(name='login_page'))
         sm.add_widget(MainPage(name='main_page'))
+        sm.add_widget(MainPage(name='main_page'))
+        sm.add_widget(Layout1Page(name='layout1_page'))
+        sm.add_widget(Layout2Page(name='layout2_page'))
+        sm.add_widget(Layout3Page(name='layout3_page'))
         sm.add_widget(AlertPage(name='alert_page'))
         sm.add_widget(AboutPage(name='about_page'))
         return sm
 
-
 if __name__ == '__main__':
     Window.clearcolor = (1, 1, 1, 1)
     AuthApp().run()
-
